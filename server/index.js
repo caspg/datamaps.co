ca96ec7169ce9f1beb2fddd5295d812850ebf8c0
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-26 22:14:48 +0100

    add handlebars

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
-const express = require('express');
+const express = require('express')
+const engines = require('consolidate')
 
-const isDev = process.env.NODE_ENV !== 'production';
-const port = isDev ? 3000 : process.env.PORT;
-const app = express();
+const isDev = process.env.NODE_ENV !== 'production'
+const port = isDev ? 3000 : process.env.PORT
+const app = express()
+
+app.engine('hbs', engines.handlebars)
+
+app.set('views', './views')
+app.set('view engine', 'hbs')
 
 app.get('/', function(req, res) {
-  res.send('Hello, world!');
-});
+  res.render('index')
+})
 
 app.listen(port, '0.0.0.0', function _onStart(err) {
   if (err) {
-    console.log(err);
+    console.log(err)
   }
-  console.info('==> 🌎  Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
-});
+  console.info('==> 🌎  Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port)
+})
