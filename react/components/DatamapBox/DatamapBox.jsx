28fdfa06ed3e53fe488aea81703f6aad30d1b049
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-27 21:24:41 +0200

    create MapElements component

--- react/components/DatamapBox/DatamapBox.jsx
+++ react/components/DatamapBox/DatamapBox.jsx
@@ -1,47 +1,29 @@
 import React, { Component } from 'react'
 
-import MapWithLegend from './MapWithLegend'
+import MapElements from './MapElements'
 
 export default class DatamapBox extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      renderMap: false,
       containerWidth: null,
     }
   }
 
   componentDidMount() {
-    this.setState({
-      containerWidth: this.refs.DatamapBox.clientWidth,
-      renderMap: true,
-    })
-  }
-
-  renderMapWithLegend(svgWidth, svgHeight) {
-    return (
-      <MapWithLegend
-        svgWidth={svgWidth}
-        svgHeight={svgHeight}
-      />
-    )
+    this.setState({ containerWidth: this.refs.DatamapBox.clientWidth })
   }
 
   render() {
     const { containerWidth } = this.state
     const svgWidth = containerWidth ? Math.min(containerWidth, 800) : 0
-    const svgHeight = svgWidth * 0.8
-    const svgStyle = {
-      width: svgWidth,
-      height: svgHeight,
-      backgroundColor: 'green',
-    }
 
     return (
       <div ref="DatamapBox">
-        <svg style={svgStyle}>
-          {this.state.renderMap && this.renderMapWithLegend(svgWidth, svgHeight)}
-        </svg>
+        <MapElements
+          svgWidth={svgWidth}
+          svgHeight={svgWidth * 0.8}
+        />
       </div>
     )
   }
---
+++ react/components/DatamapBox/MapElements.jsx
@@ -0,0 +1,37 @@
+import React, { Component, PropTypes } from 'react';
+
+import MapWithLegend from './MapWithLegend'
+
+export default class DatamapElements extends Component {
+  renderMapElements(svgWidth, svgHeight) {
+    const svgStyle = {
+      width: svgWidth,
+      height: svgHeight,
+      backgroundColor: 'green',
+    }
+
+    return (
+      <svg style={svgStyle}>
+        <MapWithLegend
+          svgWidth={svgWidth}
+          svgHeight={svgHeight}
+        />
+      </svg>
+    )
+  }
+
+  render() {
+    const { svgWidth, svgHeight } = this.props
+
+    return (
+      <div>
+        {svgWidth && svgHeight && this.renderMapElements(svgWidth, svgHeight)}
+      </div>
+    );
+  }
+}
+
+DatamapElements.propTypes = {
+  svgWidth: PropTypes.number.isRequired,
+  svgHeight: PropTypes.number.isRequired,
+}
