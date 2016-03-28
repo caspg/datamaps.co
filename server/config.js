cb096fdeb6fb8d7325103e7b84aedc25eb351f12
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-28 10:57:32 +0200

    install morgan node logger

--- server/config.js
+++ server/config.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
 const sassMiddleware = require('node-sass-middleware')
 const path = require('path')
-var expresshbs  = require('express-handlebars');
+const expresshbs = require('express-handlebars')
+
 const app = express()
+
+app.use(morgan('dev'))
 
 app.engine('hbs', expresshbs({ defaultLayout: 'default', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -11,7 +15,7 @@ app.use(sassMiddleware({
   src: path.resolve(__dirname, '../assets/scss'),
   dest: path.resolve(__dirname, '../public/css'),
   outputStyle: 'compressed',
-  prefix: '/css'
+  prefix: '/css',
 }))
 app.use('/css', express.static(path.join(__dirname, '../public/css')))
 app.use('/js', express.static(path.join(__dirname, '../public/js')))
