c7c8f681c9aea2719bb505da49c3750f98c99781
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-29 21:55:05 +0200

    highlight subunit when active

--- react/components/DatamapBox/MapWithLegend/Datamap/DatamapSubunit.jsx
+++ react/components/DatamapBox/MapWithLegend/Datamap/DatamapSubunit.jsx
@@ -1,11 +1,38 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class DatamapSubunit extends Component {
+  constructor(props) {
+    super(props)
+    this.handleMouseEnter = this.handleMouseEnter.bind(this)
+    this.handleMouseLeave = this.handleMouseLeave.bind(this)
+
+    this.state = {
+      active: false,
+    }
+  }
+
+  handleMouseEnter() {
+    this.setState({ active: true })
+  }
+
+  handleMouseLeave() {
+    this.setState({ active: false })
+  }
+
   render() {
+    const subutniStyle = {
+      fill: this.state.active ? '#FFCCBC' : '#ccc',
+      stroke: this.state.active ? '#FF5722' : '#ddd',
+      strokeWidth: this.state.active ? 2 : 0.5,
+    }
+
     return (
       <path
         className="datamap-subunit"
+        style={subutniStyle}
         d={this.props.path()}
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
       />
     );
   }
