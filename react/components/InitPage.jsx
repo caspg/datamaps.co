0ad4dec90674bc68c2fb12079173a0f9c5c07a37
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-30 22:57:29 +0200

    change marings on InitPage

--- react/components/InitPage.jsx
+++ react/components/InitPage.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router'
 export default class InitPage extends Component {
   render() {
     const btnStyle = {
-      marginTop: 10,
+      marginBottom: 20,
       minWidth: 300,
     }
 
     return (
-      <div className="row center-xs" style={{ marginTop: 150 }}>
+      <div className="row center-xs" style={{ marginTop: 30 }}>
         <div className="col-xs-12">
           <Link to="#" className="button" style={btnStyle}>Upload CSV data file</Link>
           <Link to="#" className="button" style={btnStyle}>Start with empty data table</Link>
