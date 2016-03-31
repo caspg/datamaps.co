49fe65b07baad6f0cc35f96da03a2f5a124d37be
Kacper Golinski <kacper.golinski@netguru.co>
2016-03-31 22:31:22 +0200

    add link to dropzonebox

--- react/components/DropzoneBox.jsx
+++ react/components/DropzoneBox.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Dropzone from 'react-dropzone'
+import { Link } from 'react-router'
 
 export default class DropzoneBox extends Component {
   constructor(props) {
@@ -13,7 +14,9 @@ export default class DropzoneBox extends Component {
   render() {
     return (
       <div className="dropzone-container">
-        <Dropzone className="dropzone"  activeClassName="dropzone-active" onDrop={this.onDrop}>
+        <Link to="/editor" className="button empty-table-btn">go to empty data table</Link>
+
+        <Dropzone className="dropzone" activeClassName="dropzone-active" onDrop={this.onDrop}>
           <div>Try dropping some file here, or click to select files to upload.</div>
         </Dropzone>
       </div>
