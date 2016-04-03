0980ead67368d7c94fdf54f4278864c10985ffcf
Kacper Golinski <kacper.golinski@netguru.co>
2016-04-03 22:42:49 +0200

    connect UploadPage container to store

--- react/containers/pages/UploadPage.jsx
+++ react/containers/pages/UploadPage.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import DropzoneBox from '../../components/DropzoneBox'
 
-export default class UploadPage extends Component {
+class UploadPage extends Component {
+  constructor(props) {
+    super(props)
+    this.handleDataUpload = this.handleDataUpload.bind(this)
+  }
+
+  handleDataUpload(data) {
+    console.log(data)
+  }
+
   render() {
     return (
-      <DropzoneBox />
+      <DropzoneBox
+        onDataUpload={this.handleDataUpload}
+      />
     )
   }
 }
+
+export default connect()(UploadPage)
