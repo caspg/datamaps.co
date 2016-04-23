cf2525807bee69d29c057a8b01296fff62aa542a
Kacper Golinski <kacper.golinski@netguru.co>
2016-04-23 19:22:42 +0200

    add equidistant scale data to initial state

--- react/store/initialState.js
+++ react/store/initialState.js
@@ -9,13 +9,17 @@ export default {
   regionData: statesDummyData,
   extremeValues: extremeValues,
   sortState: Map({ key: 'regionName', direction: 'ASC' }),
-  mapUi: fromJS({
+  mapUi: Map({
     title: 'Super map test title',
     dataClassification: 'linear',
-    linear: {
+    linear: Map({
       startColor: '#f7fbff',
       endColor: '#08519c',
-    },
+    }),
+    equidistant: Map({
+      classesCount: 5,
+      pallete: ['#f0f9e8', '#bae4bc', '#7bccc4', '#43a2ca', '#0868ac'],
+    }),
     noDataColor: '#f5f5f5',
   }),
 }
