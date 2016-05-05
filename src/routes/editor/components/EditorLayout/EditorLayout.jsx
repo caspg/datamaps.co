ccf5eb401f973d97155f5e8ef738869db7ab75d5
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-05 23:06:06 +0200

    add EditorLayout.css

--- src/routes/editor/components/EditorLayout/EditorLayout.jsx
+++ src/routes/editor/components/EditorLayout/EditorLayout.jsx
@@ -1,12 +1,21 @@
 import React, { PropTypes } from 'react'
 
+import style from './EditorLayout.css'
 import Header from '../Header/Header'
 
 const EditorLayout = (props) =>
   <div>
     <Header />
 
-    {props.children}
+    <div className={style.wrapper}>
+      <div className={style['map-column']}>
+        MapContainer
+      </div>
+
+      <div className={style['editor-body-column']}>
+        {props.children}
+      </div>
+    </div>
   </div>
 
 EditorLayout.propTypes = {
