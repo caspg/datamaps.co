7da184374d2dfbf085bbd095ba34fd21a79c63ae
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-05 22:41:41 +0200

    add SaveButtons component to Header

--- src/routes/editor/components/Header/Header.jsx
+++ src/routes/editor/components/Header/Header.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 import style from './Header.css'
 import Navbar from 'components/Navbar/Navbar'
+import SaveButtons from '../SaveButtons/SaveButtons'
 
 const Header = () =>
   <div>
     <div className={style['navbar-wrapper']}>
       <Navbar textColor="grey" />
+    </div>
+
+    <div className={'row middle-xs end-xs ' + style.actionbar}>
+      <SaveButtons />
     </div>
   </div>
 
