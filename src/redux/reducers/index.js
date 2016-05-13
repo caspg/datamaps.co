856a47595da844ad06ff248141ab006dae27e366
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-13 22:06:24 +0200

    delete code

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -5,13 +5,11 @@ import regionData from './regionData'
 import mapUi from './mapUi'
 import sortState from './sortState'
 import regionCodes from './regionCodes'
-import extremeValues from './extremeValues'
 
 export default combineReducers({
   regionData,
   mapUi,
   sortState,
   regionCodes,
-  extremeValues,
   routing: routerReducer,
 })
