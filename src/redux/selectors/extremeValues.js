c3f65852f763b5644a3951e24a76a69dc2ef3060
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-16 20:33:50 +0200

    update extremeValues selector

--- src/redux/selectors/extremeValues.js
+++ src/redux/selectors/extremeValues.js
@@ -1,18 +1,24 @@
-import { Map } from 'immutable'
+import { Map, List } from 'immutable'
 import { createSelector } from 'reselect'
 
 const replaceUndefined = (value) =>
   (typeof value === 'undefined') ? '' : value
 
 const getRegionData = (state) => state.regionData
+const domainStartValue = (state) => state.mapUi.getIn(['equidistant', 'domainStartValue'])
+const domainEndValue = (state) => state.mapUi.getIn(['equidistant', 'domainEndValue'])
 
 const makeExtremeValuesSelector = () =>
-  createSelector([getRegionData], (regionData) => {
+  createSelector(
+  [getRegionData, domainStartValue, domainEndValue],
+  (regionData, startValue, endValue) => {
     const values = regionData.map((item) => item.get('value'))
-    const filteredValues = values.filterNot((item) => item === '')
+    const combinedValues = List([values, startValue, endValue]).flatten()
+    const filteredValues = combinedValues.filterNot((item) => item === '')
+
     return Map({
       min: replaceUndefined(filteredValues.min()),
-      max: replaceUndefined(filteredValues.max())
+      max: replaceUndefined(filteredValues.max()),
     })
   }
 )
