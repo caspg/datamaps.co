9db14b6a55f04552bc15af3546d93b9032436afa
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-18 22:39:30 +0200

    add "save svg" button

--- src/routes/editor/components/SaveButtons/SaveButtons.jsx
+++ src/routes/editor/components/SaveButtons/SaveButtons.jsx
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
-import { saveSvgAsPng } from 'save-svg-as-png'
+import { svgAsDataUri, saveSvgAsPng } from 'save-svg-as-png'
 
 import style from './SaveButtons.css'
 
 export default class SaveButtons extends Component {
   handleSavePng() {
     const options = { scale: 3, backgroundColor: 'white' }
-    saveSvgAsPng(document.getElementsByTagName('svg')[0], 'datamapsco.png', options);
+    saveSvgAsPng(document.getElementsByTagName('svg')[0], 'datamapsco', options);
+  }
+
+  handleSaveSvg() {
+    const a = document.createElement('a')
+    const svg = document.getElementsByTagName('svg')[0]
+    svgAsDataUri(svg, {}, (uri) => { a.href = uri })
+
+    a.download = 'datamapsco'
+    document.body.appendChild(a)
+    a.addEventListener('click', () => a.parentNode.removeChild(a))
+    a.click()
   }
 
   render() {
     return (
       <div>
-        <button className={'button ' + style['png-btn']} onClick={this.handleSavePng}>Save png</button>
+        <button className={'button ' + style['save-btn']} onClick={this.handleSaveSvg}>Save svg</button>
+        <button className={'button ' + style['save-btn']} onClick={this.handleSavePng}>Save png</button>
       </div>
     )
   }
