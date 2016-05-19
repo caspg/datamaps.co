ee56cd432c89f3fbe2c6e0073c54334c50a17b1c
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-19 19:52:19 +0200

    add DevTools

--- src/index.jsx
+++ src/index.jsx
@@ -10,16 +10,20 @@ import { syncHistoryWithStore } from 'react-router-redux'
 import configureStore from './redux/store/configureStore'
 import HomeRoute from './routes/home/HomeRoute'
 import EditorRoute from './routes/editor/EditorRoute'
+import Devtools from './containers/Devtools'
 
 const store = configureStore()
 const history = syncHistoryWithStore(browserHistory, store)
 
 const Root = () =>
   <Provider store={store}>
-    <Router history={history}>
-      {HomeRoute}
-      {EditorRoute}
-    </Router>
+    <div>
+      <Router history={history}>
+        {HomeRoute}
+        {EditorRoute}
+      </Router>
+      <Devtools />
+    </div>
   </Provider>
 
 render(
