a9bcfdbd5f5d1adbb9176fa57ca5bdafefad1167
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-23 17:01:41 +0200

    prod setup

---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+lock '3.5.0'
+
+set :application, 'deltamike_node'
+set :repo_url,    'git@bitbucket.org:caspg/deltamike-node.git'
+set :user,        'caspg'
+
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+
+set :linked_dirs, %w(
+  node_modules,
+  log
+)
+
+namespace :deploy do
+  desc 'Make sure local git is in sync with remote.'
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/production`
+        puts 'WARNING: HEAD is not the same as origin/production'
+        puts 'Run `git push` to sync changes.'
+        exit
+      end
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app) do
+      execute(:npm, :run, 'restart-server-prod')
+    end
+  end
+
+  desc 'Start application'
+  task :start do
+    on roles(:app) do
+      execute(:npm, :run, 'start-server-prod')
+    end
+  end
+
+  desc 'Build react file' do
+    task :build do
+      on roles(:app) do
+        execute(:npm, :run, 'build')
+      end
+    end
+  end
+
+  before :starting, :check_revision
+  after :finishing, :restart
+  after :finishing, :build
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,5 @@
+server '51.254.124.221', port: 966, roles: [:web, :app, :db], primary: true
+
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :stage,       :production
+set :branch,      'production'
