291e8ae3155aadb638d9ace2148a9cf6e13dc103
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-23 17:25:38 +0200

    prod setup

--- server/config.js
+++ server/config.js
@@ -2,10 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
 
-const webpackConfig = require('../webpack/config.js')
-const webpack = require('webpack')
-const compiler = webpack(webpackConfig)
-
 const app = express()
 
 app.use(morgan('dev'))
@@ -16,6 +12,9 @@ app.use('/data', express.static(path.join(__dirname, '../public/data')))
 
 if (process.env.NODE_ENV !== 'production') {
   const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackConfig = require('../webpack/config.js')
+  const webpack = require('webpack')
+  const compiler = webpack(webpackConfig)
 
   app.use(webpackDevMiddleware(compiler, {
     hot: true,
