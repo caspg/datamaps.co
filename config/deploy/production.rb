1a914fecd75a93f5c16f6714564b10e768d2d670
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-23 17:01:59 +0200

    prod setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ server '51.254.124.221', port: 966, roles: [:web, :app, :db], primary: true
 
 set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :stage,       :production
-set :branch,      'production'
+set :branch,      'master'
