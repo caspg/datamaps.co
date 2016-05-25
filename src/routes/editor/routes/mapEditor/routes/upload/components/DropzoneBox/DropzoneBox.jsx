4367892cc5edaab0cf6f8920c9f8f6468b95fab3
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-25 17:16:33 +0200

    validate uploaded CSV file columns

--- src/routes/editor/routes/mapEditor/routes/upload/components/DropzoneBox/DropzoneBox.jsx
+++ src/routes/editor/routes/mapEditor/routes/upload/components/DropzoneBox/DropzoneBox.jsx
@@ -9,6 +9,9 @@ export default class DropzoneBox extends Component {
   constructor(props) {
     super(props)
     this.onDrop = this.onDrop.bind(this)
+    this.state = {
+      errors: [],
+    }
   }
 
   onDrop(files) {
@@ -16,29 +19,62 @@ export default class DropzoneBox extends Component {
     const reader = new FileReader()
     reader.onload = (e) => {
       const parsedCSV = d3.csv.parse(e.target.result)
-      const list = fromJS(parsedCSV)
-      const filteredList = list.filter((item) => item.get('value'))
-      const parsedList = filteredList.map((item) =>
-        item.update((_item) => _item.set('value', parseFloat(_item.get('value'))))
-      )
+      const columns = Object.keys(parsedCSV[0])
 
-      this.props.onDataUpload(parsedList)
+      if (this.validateColumnNames(columns)) {
+        const list = fromJS(parsedCSV)
+        const filteredList = list.filter((item) => item.get('value'))
+        const parsedList = filteredList.map((item) =>
+          item.update((_item) => _item.set('value', parseFloat(_item.get('value'))))
+        )
+
+        this.props.onDataUpload(parsedList)
+      }
     }
 
     reader.readAsText(file)
   }
 
+  validateColumnNames(columns) {
+    const errors = []
+
+    if (columns.indexOf('value') === -1) {
+      errors.push('CSV file has to contain "value" column')
+    }
+
+    if (columns.indexOf('code') === -1 && columns.indexOf('name') === -1) {
+      errors.push('CSV file has to contain "code" or "name" column')
+    }
+
+    if (errors.length > 0) {
+      this.setState({ errors })
+      return false
+    }
+
+    return true
+  }
+
+  renderErrors() {
+    const errors = this.state.errors.map((error, index) => <li key={index}>{error}</li>)
+
+    return errors.length > 0 ? (<ul className={style.errors}>{errors}</ul>) : ''
+  }
+
   render() {
     return (
-      <Dropzone
-        className={style.dropzone}
-        activeClassName={style['dropzone-active']}
-        onDrop={this.onDrop}
-      >
-        <div>
-          Try dropping some file here, or click to select files to upload.
-        </div>
-      </Dropzone>
+      <div>
+        {this.renderErrors()}
+
+        <Dropzone
+          className={style.dropzone}
+          activeClassName={style['dropzone-active']}
+          onDrop={this.onDrop}
+        >
+          <div>
+            Try dropping some file here, or click to select files to upload.
+          </div>
+        </Dropzone>
+      </div>
     )
   }
 }
