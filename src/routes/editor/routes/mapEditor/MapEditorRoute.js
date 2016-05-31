86d841e2a4a099adbd4ce0e40ddc2c191b9e8860
Kacper Golinski <kacper.golinski@netguru.co>
2016-05-31 18:13:09 +0200

    update MapEditorRoute.js

--- src/routes/editor/routes/mapEditor/MapEditorRoute.js
+++ src/routes/editor/routes/mapEditor/MapEditorRoute.js
@@ -4,13 +4,14 @@ import { Route, IndexRoute } from 'react-router'
 import MapEditorContainer from './containers/MapEditorContainer'
 import InitView from './components/InitView/InitView'
 
+import mapConfig from 'config/maps'
 import routes from 'config/routes'
 import UploadRoute from './routes/upload/UploadRoute'
 import EditDataRoute from './routes/editData/EditDataRoute'
 import EditMapRoute from './routes/editMap/EditMapRoute'
 
 const validateParam = (nextState, replace) => {
-  const mapTypes = ['usa', 'world']
+  const mapTypes = mapConfig.types.map(i => i.code)
 
   if (mapTypes.indexOf(nextState.params.mapType) < 0) {
     replace({
