8dac7e15411cf020e8cc37bfb1f0ba281e982c03
Kacper Golinski <kacper.golinski@gmail.com>
2016-11-01 22:22:54 +0000

    update Header content

--- src/routes/home/components/Header/Header.jsx
+++ src/routes/home/components/Header/Header.jsx
@@ -3,13 +3,28 @@ import React, { PropTypes } from 'react'
 import style from './Header.css'
 
 const Header = (props) =>
-  <header className={style.header}>
+  <header>
     {props.children}
 
-    <div className={'row middle-xs center-xs ' + style['title-container']}>
-      <h1 className={style.title}>
-        Create and download data maps.
-      </h1>
+    <div className={`row middle-sm center-xs ${style['header-content']}`}>
+
+      <div className="col-sm-6 col-xs-12">
+        <div className="row end-sm center-xs ">
+
+
+          <div className={style['title-container']}>
+            <h1 className={style.title}>
+              Create your free data map of world
+            </h1>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="col-sm-6 col-xs-12">
+        <img className={style.image} src="/images/editor-screen.png" alt="datamaps-editor" />
+      </div>
+
     </div>
   </header>
 
