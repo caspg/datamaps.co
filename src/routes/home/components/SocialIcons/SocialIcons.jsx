d8576be4b6b384ac604c94c03d4ef873299e5bc3
Kacper Golinski <kacper.golinski@gmail.com>
2016-11-06 19:00:11 +0000

    add github icon

--- src/routes/home/components/SocialIcons/SocialIcons.jsx
+++ src/routes/home/components/SocialIcons/SocialIcons.jsx
@@ -4,13 +4,19 @@ import style from './SocialIcons.css'
 
 const TwitterIcon = () =>
   <a href="https://twitter.com/datamapsco">
-    <img className={style.twittericon} src="/images/icons/twitter-64.png" alt="twitter icon" />
+    <img className={style.icon} src="/images/icons/twitter-64.png" alt="twitter icon" />
+  </a>
+
+const GithubIcon = () =>
+  <a href="https://github.com/caspg/datamaps.co">
+    <img className={style.icon} src="/images/icons/github-64.png" alt="github icon" />
   </a>
 
 const SocialIcons = () =>
   <div className={style.container}>
     <div className="row middle-xs center-xs">
       <TwitterIcon />
+      <GithubIcon />
     </div>
   </div>
 
