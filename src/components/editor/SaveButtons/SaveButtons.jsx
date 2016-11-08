82903febe7db46f50be37d399eb7a83cdf79f0c0
Kacper Golinski <kacper.golinski@gmail.com>
2016-11-08 21:37:52 +0000

    move SaveButtons logic to external module

--- src/components/editor/SaveButtons/SaveButtons.jsx
+++ src/components/editor/SaveButtons/SaveButtons.jsx
@@ -1,58 +1,27 @@
-import React, { Component, PropTypes } from 'react'
-import { svgAsDataUri, saveSvgAsPng } from 'save-svg-as-png'
+import React, { PropTypes } from 'react'
 
+import { downloadPng, downloadSvg } from 'utils/downloadMap'
 import style from './SaveButtons.css'
 
-export default class SaveButtons extends Component {
-  constructor(props) {
-    super(props)
-    this.handleSaveSvg = this.handleSaveSvg.bind(this)
-    this.handleSavePng = this.handleSavePng.bind(this)
-  }
+const SaveButtons = props =>
+  <div>
+    <button
+      className={`button ${style['save-btn']}`}
+      onClick={() => downloadSvg(props.mapType)}
+    >
+      Save svg
+    </button>
 
-  trackEvent(type) {
-    console.log(this.props.mapType)
-    if (process.env.NODE_ENV !== 'production') return
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Save Button',
-      eventAction: type,
-      eventLabel: this.props.mapType,
-    });
-  }
-
-  handleSavePng() {
-    const gElement = document.getElementById('root-svg-group')
-    const scaleFactor = gElement.getBoundingClientRect().width / gElement.getBBox().width
-    const scale = 3 * scaleFactor
-
-    const options = { scale: scale, backgroundColor: 'white' }
-    saveSvgAsPng(document.getElementsByTagName('svg')[0], 'datamapsco', options);
-    this.trackEvent('PNG')
-  }
-
-  handleSaveSvg() {
-    const a = document.createElement('a')
-    const svg = document.getElementsByTagName('svg')[0]
-    svgAsDataUri(svg, {}, (uri) => { a.href = uri })
-
-    a.download = 'datamapsco'
-    document.body.appendChild(a)
-    a.addEventListener('click', () => a.parentNode.removeChild(a))
-    a.click()
-    this.trackEvent('SVG')
-  }
-
-  render() {
-    return (
-      <div>
-        <button className={'button ' + style['save-btn']} onClick={this.handleSaveSvg}>Save svg</button>
-        <button className={'button ' + style['save-btn']} onClick={this.handleSavePng}>Save png</button>
-      </div>
-    )
-  }
-}
+    <button
+      className={`button ${style['save-btn']}`}
+      onClick={() => downloadPng(props.mapType)}
+    >
+      Save png
+    </button>
+  </div>
 
 SaveButtons.propTypes = {
   mapType: PropTypes.string.isRequired,
 }
+
+export default SaveButtons
---
+++ src/utils/downloadMap.js
@@ -0,0 +1,34 @@
+import { svgAsDataUri, saveSvgAsPng } from 'save-svg-as-png'
+
+const trackEvent = (type, mapType) => {
+  if (process.env.NODE_ENV !== 'production') return
+
+  window.ga('send', {
+    hitType: 'event',
+    eventCategory: 'Save Button',
+    eventAction: type,
+    eventLabel: mapType,
+  });
+}
+
+export const downloadPng = (mapType) => {
+  const gElement = document.getElementById('root-svg-group')
+  const scaleFactor = gElement.getBoundingClientRect().width / gElement.getBBox().width
+  const scale = 3 * scaleFactor
+
+  const options = { scale, backgroundColor: 'white' }
+  saveSvgAsPng(document.getElementsByTagName('svg')[0], 'datamapsco', options);
+  trackEvent('PNG', mapType)
+}
+
+export const downloadSvg = (mapType) => {
+  const a = document.createElement('a')
+  const svg = document.getElementsByTagName('svg')[0]
+  svgAsDataUri(svg, {}, (uri) => { a.href = uri })
+
+  a.download = 'datamapsco'
+  document.body.appendChild(a)
+  a.addEventListener('click', () => a.parentNode.removeChild(a))
+  a.click()
+  this.trackEvent('SVG', mapType)
+}
