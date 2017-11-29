fca5ecdf5693de0909e39f7aa38eabf4dc250a96
Kacper Golinski <kacper.golinski@gmail.com>
2017-11-29 14:04:51 +0100

    create pages directories

--- src/config/routes.js
+++ src/config/routes.js
@@ -9,7 +9,7 @@ export default {
   editData:  '/editor/:mapType/edit-data',
   editMap:   '/editor/:mapType/edit-map',
 
-  clickEditor:   '/click-editor',
+  clickEditor:    '/click-editor',
   clickMapEditor: '/click-editor/:mapType',
 
   contact: '/contact',
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Home.propTypes = {
+
+};
+
+function Home(props) {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default Home;
