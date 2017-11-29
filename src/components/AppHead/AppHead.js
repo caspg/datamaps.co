837ec4e9a3978bc83aad3064616de1f542fd68f0
Kacper Golinski <kacper.golinski@gmail.com>
2017-11-29 19:53:40 +0100

    move favicon to static/

--- src/components/AppHead/AppHead.js
+++ src/components/AppHead/AppHead.js
@@ -20,9 +20,14 @@ function AppHead(props) {
       <meta name="msvalidate.01" content="E13826D612F7D8D97C50E1DB61BF34C6" />
       <meta name="description" content={props.description} />
 
-      <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css" />
-      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      <link
+        href="//fonts.googleapis.com/css?family=Raleway:400,300,600"
+        rel="stylesheet"
+        type="text/css"
+      />
+
+      <link rel="shortcut icon" href="/static/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="/static/favicon.ico" type="image/x-icon" />
 
       <link href="/static/css/vendors/normalize.css" rel="stylesheet" type="text/css" />
       <link href="/static/css/vendors/flexboxgrid.min.css" rel="stylesheet" type="text/css" />
