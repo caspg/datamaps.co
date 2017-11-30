6725ecc74a229e589dedfddf1a3f4a163f0c5038
Kacper Golinski <kacper.golinski@gmail.com>
2017-11-30 19:49:25 +0100

    remove webpack configs

--- webpack/prod.config.js
+++
@@ -1,8 +0,0 @@
-const Webpack = require('webpack')
-const config = require('./config')
-
-config.plugins.push(
-  new Webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': '"production"' } })
-)
-
-module.exports = config
