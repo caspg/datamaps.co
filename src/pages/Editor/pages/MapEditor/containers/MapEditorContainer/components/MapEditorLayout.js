2280e7b59cba2976a428cfb4a0ada7075bcb0f7c
Kacper Golinski <kacper.golinski@gmail.com>
2017-12-14 14:22:56 +0100

    update MapEditorLayout.js

--- src/pages/Editor/pages/MapEditor/containers/MapEditorContainer/components/MapEditorLayout.js
+++ src/pages/Editor/pages/MapEditor/containers/MapEditorContainer/components/MapEditorLayout.js
@@ -1,27 +1,75 @@
 import React, { PropTypes } from 'react'
 
-// import style from './MapEditorLayout.css'
 // import MapContainer from 'containers/editor/MapContainer'
+
+import { mapEditorLayoutZIndex } from '@src/styles/variables';
+import { grey300 } from '@src/styles/colors';
 import Footer from '@src/components/Footer'
 
-const style = {};
+const editorBodyWidth = '400px';
 
-const MapEditorLayout = (props) =>
-  <div className={style.container}>
-    <div className={style['map-column']}>
-      {/* {props.isDataReady && <MapContainer />} */}
+function MapEditorLayout(props) {
+  return (
+    <div className="MapEditorLayout">
+      <div className="MapEditorLayout__map-column">
+        {/* {props.isDataReady && <MapContainer />} */}
+      </div>
+
+      <div className="MapEditorLayout__body-column">
+        {props.isDataReady && props.children}
+
+        <Footer>
+          <i>
+            Color schemes from <a style={{ color: 'inherit' }} href="http://www.colorbrewer.org" title="colorbrewer" target="_blank">colorbrewer.org</a>
+          </i>
+        </Footer>
+      </div>
+
+      <style jsx>{`
+        .MapEditorLayout {
+          margin-top: 100px;
+          position: relative;
+          z-index: ${mapEditorLayoutZIndex};
+        }
+
+        .MapEditorLayout__map-column {
+          height: 600px;
+          padding-bottom: 20px;
+          overflow: scroll;
+        }
+
+        @media(min-width: 950px) {
+          .MapEditorLayout {
+            margin-top: 102px;
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            overflow: hidden;
+          }
+
+          .MapEditorLayout__map-column {
+            margin-right: ${editorBodyWidth};
+            height: 100%;
+            border-bottom: 1px solid ${grey300};
+          }
+
+          .MapEditorLayout__body-column {
+            width: ${editorBodyWidth};
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            right: 0;
+            border-left: 1px solid ${grey300};
+            overflow-y: scroll;
+            overflow-x: hidden;
+          }
+        }
+      `}</style>
     </div>
-
-    <div className={style['editor-body-column']}>
-      {props.isDataReady && props.children}
-
-      <Footer>
-        <i>
-          Color schemes from <a style={{ color: 'inherit' }} href="http://www.colorbrewer.org" title="colorbrewer" target="_blank">colorbrewer.org</a>
-        </i>
-      </Footer>
-    </div>
-  </div>
+  )
+}
 
 MapEditorLayout.propTypes = {
   isDataReady: PropTypes.bool.isRequired,
