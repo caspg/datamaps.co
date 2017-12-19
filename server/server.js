9e329f8ce2de5dcb58ec316bef764c4c091340b4
Kacper Golinski <kacper.golinski@gmail.com>
2017-12-19 22:00:14 +0100

    update server formatting

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const { createServer } = require('http')
 const next = require('next')
+
 const routes = require('./routes')
 
 const port = parseInt(process.env.PORT, 10) || 3000
@@ -10,11 +11,11 @@ const handler = routes.getRequestHandler(app)
 app.prepare()
 .then(() => {
   createServer(handler)
-  .listen(port, (err) => {
-    if (err) {
-      throw err
-    }
+    .listen(port, (err) => {
+      if (err) {
+        throw err
+      }
 
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+      console.log(`> Ready on http://localhost:${port}`)
+    })
 })
